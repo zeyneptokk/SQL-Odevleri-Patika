db5d2cf49208a796fd207f12f99804022b896d6b
zeynep yünsel <90848984+zeyneptokk@users.noreply.github.com>
2024-07-26 14:32:05 +0300

    Soru 2 eklendi

--- Odev7.sql
+++ Odev7.sql
@@ -2,3 +2,9 @@ Soru 1)film tablosunda bulunan filmleri rating değerlerine göre gruplayınız.
 SELECT rating, COUNT(*)
 FROM film
 GROUP BY rating
+
+Soru 2)film tablosunda bulunan filmleri replacement_cost sütununa göre grupladığımızda film sayısı 50 den fazla olan replacement_cost değerini ve karşılık gelen film sayısını sıralayınız.
+SELECT replacement_cost, COUNT(*)
+FROM film 
+GROUP BY replacement_cost HAVING COUNT(*) > 50
+
